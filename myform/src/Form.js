81b477a8c1798a7b95e41d1c44fc6f5af9d49863
Dino-Muratovic <dinomuratovic1989@gmail.com>
2020-03-25 20:37:52 -0500

    managed to complete the project for the day and meet the MVP requirements

--- myform/src/Form.js
+++ myform/src/Form.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from 'react'; // import hooks
 import * as yup from 'yup'; // yup for everything
+import axios from 'axios';
 
 
 // Setting up our Schema
+
+//Set up what the use is required or not required to input
 const formSchema = yup.object().shape({
    name: yup.string().required('Name is required'),
    email: yup.string().email().required('Must include the email'),
@@ -44,13 +47,63 @@ useEffect(() => {
 /* We pass the entire state into the entire schema, no need to use reach here. 
     We want to make sure it is all valid before we allow a user to submit
     isValid comes from Yup directly */   
-    formSchema.isValid(formState)
+
+
+    //make sure that formState is following all the rules coming the formSchema
+    formSchema.isValid(formState) 
+
+    //if everything is inside the form as indicated on the previous line than light up the button
     .then(valid => {
         setButtonDisabled(!valid);
-        console.log(valid);
+        // console.log(valid);
     });
 
 }, [formState]) // dependancy array - watch for formState if it changes
+
+// if dependancy array isnt watching for changes the button will not light up. if we have formState inside of that array it will.
+
+
+
+const validateChange = event => {
+     // Reach will allow us to "reach" into the schema and test only one part.
+     yup
+     .reach(formSchema, event.target.name)
+     .validate(event.target.value)
+     .then(valid =>{
+         setErrors({
+             ...errors, 
+             [event.target.name]: ""
+         });
+     }).catch(err => {
+         setErrors({
+             ...errors, [event.target.name]: err.errors
+             
+         });
+     });
+};
+
+
+//Form submit here
+const formSubmit = event => {
+    event.preventDefault();
+    
+    axios
+    .post('https://reqres.in/api/users', formState)
+    .then(res => {
+        setPost(res.data);
+        console.log("success", post)
+
+        setFormState({
+            name:"",
+            email:"",
+            password:"",
+            terms:""
+        });
+    })
+    .catch(err => {
+        console.log(err.res);
+    })
+}
 
 
 //Event handler Input change
@@ -59,21 +112,20 @@ const inputChange = event => {
     We need to be able to use it after the form validation */
     event.persist(); // QUESTION
     // console.log(`this is syntethic event `, event); 
-    console.log(`event target`, event.target)// ---------- // this returns that particular input element
-    console.log(`event target name`, event.target.name)// ----------- // this returns the key of name
-    console.log(`event target value`, event.target.value)// ---------- // this returns whatever value typed into that key example (name: --->'whatever typed in here'<---)
-
-
+    // console.log(`event target`, event.target)// ---------- // this returns that particular input element
+    // console.log(`event target name`, event.target.name)// ----------- // this returns the key of name
+    // console.log(`event target value`, event.target.value)// ---------- // this returns whatever value typed into that key example (name: --->'whatever typed in here'<---)
     const newFormData = {
         ...formState,
         [event.target.name]:
-        event.target.type === "checkbox" ? event.target.checked:event.target.value
+        event.target.type === "checkbox" ? event.target.checked : event.target.value
     };      
-    
+    validateChange(event);
+    setFormState(newFormData);
 }
 
     return (
-        <form>
+        <form onSubmit={formSubmit}>
 
             <label htmlFor="name">Name</label>
                 <input 
@@ -110,15 +162,10 @@ const inputChange = event => {
                     checked={formState.terms} // formState is our State set up for an example formState={ using name:"" , email:"", ----->password:""<-----} accessing with dot notation
                     onChange={inputChange}
              /> <br/>
-
-            <button>
+            <pre>{JSON.stringify(post, null, 2)}</pre>
+            <button disabled={buttonDisabled}>
                Submit
-            </button>
-
-
-
-
-            
+            </button>            
             
         </form>
     )
